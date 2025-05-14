62e00521b4159c0e01bddd144fe8c366da32ef1f
Prasanna <prasanna.131990@gmail.com>
2025-05-14 23:17:47 +0200

    enforce PR

--- src/main/java/com/example/restdemo/HelloController.java
+++ src/main/java/com/example/restdemo/HelloController.java
@@ -5,7 +5,7 @@ public class HelloController {
 
     @org.springframework.web.bind.annotation.GetMapping("/hello")
     public String hello() {
-        return "Hello, Spring boot with enforce PR !";
+        return "Hello, Spring boot!!";
     }
 
 }
