7281c6e8698cda96c121b92688304a7e46e3b421
Prasanna <prasanna.131990@gmail.com>
2025-05-14 22:40:04 +0200

    Initial commit

---
+++ src/main/java/com/example/restdemo/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.restdemo;
+
+@org.springframework.web.bind.annotation.RestController
+public class HelloController {
+
+    @org.springframework.web.bind.annotation.GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+}
